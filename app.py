a5174001eddcddad39476db288ac57ad9bc7dea9
tuttlepower <tatuttle2015@gmail.com>
2021-05-28 19:03:03 -0400

    lux implementation

--- app.py
+++ app.py
@@ -3,81 +3,13 @@ import random
 import Paper
 from articleDAO import getArticles, getNasa, getArxivFeed
 
-#
-from flask_sqlalchemy import SQLAlchemy 
-from sqlalchemy import func
-
 
 app = Flask(__name__)
 
-#
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///articlesDB.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-db = SQLAlchemy(app)
-
-#
-class Article(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    article = db.Column(db.String(1000))
-
-    def __repr__(self):
-        return self.id + "-" + self.article
-
-    def to_string(self):
-        return str(self.article)
-
-
-@app.route("/article", methods = ['GET'])
-def get_rand_article():
-    #get # of articles as int
-    max_number = db.session.query(func.max(Article.id)).scalar()
-    
-    #grab random ID in range
-    article_id = random.randint(0,max_number)
-
-    # get article list
-    random_article_query_object = Article.query.filter_by(id=article_id)
-
-    # get list from object
-    random_article_list = random_article_query_object.all()
-
-    # filter this list to the 0th
-    random_article_object = random_article_list[0]
-
-    # bunch of stuff
-    random_article = random_article_object.article
-
-    # or do all this bullshit in one line
-    #random_article = Article.query.all()[article_id].article
-
-    return random_article
-
-@app.route("/initialize", methods=['GET'])
-def initialize():
-    #instance db instance
-    db.create_all()
-    #iterate through articles file and add them to db
-    counter = 0 
-    with open('articles.txt')as input_file:
-        for line in input_file:
-            # create new instance of an article and pefine the values
-            article = Article()
-            article.id = counter
-            article.article = line
-
-            #add to db and commit
-            db.session.add(article)
-            db.session.commit()
-
-            counter = counter + 1
-        # flush connection
-        db.session.flush()
-
-    return "db created"
-
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", count=12)
+
 
 
 @app.route("/economics")
@@ -96,12 +28,12 @@ def econ():
     return render_template("topic.html", topic='Economics', articles=articles)
 
 
+
 @app.route("/machinelearning")
 def machineLearning():
     url = "https://jmlr.org/jmlr.xml"
     articles = getArticles(url)
     return render_template("topic.html", topic='ML/AI', articles=articles)
-
 
 @app.route("/computerscience")
 def computerScience():
